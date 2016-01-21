c4d5073ff68bf71ae3b3907c9f86df10ba0fcbf2
Hyunju Choi <hj.choi@yellotravel.com>
2016-01-21 16:06:25 +0900

    불필요한 파일 제거

--- src/archive/constants/common.js
+++
@@ -1,5 +0,0 @@
-export const VisibilityFilters = {
-    SHOW_ALL: "SHOW_ALL",
-    SHOW_COMPLETE: "SHOW_COMPLETE",
-    SHOW_ACTIVE: "SHOW_ACTIVE"
-};
--- src/archive/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import { rootReducer } from '../reducers/index';
-
-let configureStore = (initialState) => {
-    return createStore(rootReducer, initialState);
-};
-
-export { configureStore };
